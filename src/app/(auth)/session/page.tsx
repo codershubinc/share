447383786a4b2acfffe24b97e203bf90c5a62656
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-03-18 00:41:24 +0530

    Refactor session handling to use Next.js search parameters and streamline session creation

--- src/app/(auth)/session/page.tsx
+++ src/app/(auth)/session/page.tsx
@@ -1,36 +1,27 @@
 'use client'
 import createSession from '@/utils/codershubinc/createSession'
+import { useSearchParams } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 function Page() {
 
-    const params = new URLSearchParams(window.location.search)
-    const secret = params.get('secret')
-    const userId = params.get('userId')
+    const searchParams = useSearchParams()
+    const secret = searchParams.get('secret')
+    const userId = searchParams.get('userId')
 
     console.log('secrete', secret)
     console.log('userId', userId);
 
-
-    useEffect(() => {
-        if (!secret || !userId) return console.log('no secret and userId found')
-        session()
-    }, [])
     const session = async () => {
         try {
-            if (!secret || !userId) return console.log('no secret and userId found')
-            const responce = await createSession(
-                secret,
-                userId
-            )
-            console.log('response session ', responce);
+            if (!secret || !userId) return console.log('no secret and userId found');
+            const response = await createSession(secret, userId);
+            console.log('response session:', response);
         } catch (error) {
             console.log('error at creating session', error);
-            throw error
         }
-    }
-
-
+    };
+    (() => session())()
 
 
     return (
